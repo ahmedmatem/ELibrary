d1a8c8b3416cf4ef0d45d136ce56e580bb9b0abd
Ahmed Matem <ahmedmatem@gmail.com>
2025-08-03 22:57:25 +0300

    Create ResourceServiceTests skeleton

---
+++ ELibrary.Tests/Services/ResourceServiceTests.cs
@@ -0,0 +1,41 @@
+﻿namespace ELibrary.Core.Services.Tests
+{
+    public class ResourceServiceTests
+    {
+        [Fact()]
+        public void AddAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DeleteAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetAllAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetByIdAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SearchAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void UpdateAsyncTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
