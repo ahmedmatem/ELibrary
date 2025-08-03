1320635060a08ebe2d8125c46b3edd7743c74794
Ahmed Matem <ahmedmatem@gmail.com>
2025-08-03 15:02:07 +0300

    Add Azure Blobs Service intefaces

---
+++ ELibrary.Core/Contracts/IAzureBlobService.cs
@@ -0,0 +1,13 @@
+﻿using Azure;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace ELibrary.Core.Contracts
+{
+    public interface IAzureBlobService
+    {
+        Task<Response<BlobContentInfo>> UploadFileAsync(IFormFile file, string blobName);
+
+        Task<Response<bool>> DeleteAsync(string blobName);
+    }
+}
