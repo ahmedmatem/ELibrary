97877c0c7d72032e6f2575d2022236cb43f491a7
Ahmed Matem <ahmedmatem@gmail.com>
2025-08-18 00:00:04 +0300

    Add IDesignTimeDbContextFactory<ApplicationDbContext> that allows dotnet ef to instantiate your DbContext manually and pass in the connection string you're giving with --connection.

---
+++ ELibrary.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ELibrary.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Pull connection string from env var passed by GitHub Actions
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("CONNECTION_STRING environment variable is not set.");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
