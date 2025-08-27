5e9123aa2ac1e1146f15388c71942d69e6cd7c38
Ahmed Matem <ahmedmatem@gmail.com>
2025-08-27 23:09:32 +0300

    Redirect to specific area depends on user role after successful login

---
+++ ELibrary.Infrastructure/Data/Types/AppRole.cs
@@ -0,0 +1,9 @@
+﻿namespace ELibrary.Infrastructure.Data.Types
+{
+    public static class AppRole
+    {
+        public const string ADMIN = "Admin";
+        public const string TEACHER = "Teacher";
+        public const string STUDENT = "Student";
+    }
+}
--- ELibrary.Web/Controllers/HomeController.cs
+++ ELibrary.Web/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-using System.ComponentModel.Design;
 using System.Diagnostics;
-using ELibrary.Web.Models;
+
 using Microsoft.AspNetCore.Mvc;
+
+using ELibrary.Web.Models;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Identity;
+using ELibrary.Infrastructure.Data.Types;
 
 namespace ELibrary.Web.Controllers
 {
@@ -16,7 +20,22 @@ namespace ELibrary.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            if (User.IsInRole(AppRole.ADMIN))
+            {
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            else if(User.IsInRole(AppRole.TEACHER))
+            {
+                return RedirectToAction("Index", "Home", new { area = "Teacher" });
+            }
+            else if (User.IsInRole(AppRole.STUDENT))
+            {
+                return RedirectToAction("Index", "Home", new { area = "Student"});
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public IActionResult Privacy()
