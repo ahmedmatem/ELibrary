bffafbd0efe9645bfdc1a058c7f66926589ba20e
Ahmed Matem <ahmedmatem@gmail.com>
2025-08-27 23:41:26 +0300

    Add Admin Home Controller and Index Action/View

---
+++ ELibrary.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using ELibrary.Infrastructure.Data.Types;
+
+namespace ELibrary.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AppRole.ADMIN)]
+    public class AdminBaseController : Controller { }
+}
