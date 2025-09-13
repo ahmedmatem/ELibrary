1e6d5304e3dacdd6667e8ddc2366745caf684505
Ahmed Matem <ahmedmatem@gmail.com>
2025-09-13 21:49:40 +0300

    Change Id type from Guid to string

--- ELibrary.Core/DTOs/ResourceDto.cs
+++ ELibrary.Core/DTOs/ResourceDto.cs
@@ -8,7 +8,7 @@
     /// details.</remarks>
     public class ResourceDto
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; } = "";
         public string Subject { get; set; } = "";
         public string Description { get; set; } = "";
--- ELibrary.Web/Models/ResourceViewModel.cs
+++ ELibrary.Web/Models/ResourceViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class ResourceViewModel
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; } = "";
         public string Subject { get; set; } = "";
         public string Description { get; set; } = "";
