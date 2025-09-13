88a0bf85a58fd27323658e4d0ffa15b0256d4e5d
Ahmed Matem <ahmedmatem@gmail.com>
2025-09-13 21:48:32 +0300

    Add Automapper mapping profile file to Core module

---
+++ ELibrary.Core/Automapper/CoreMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ELibrary.Core.DTOs;
+using ELibrary.Infrastructure.Data.Models;
+
+namespace ELibrary.Core.Automapper
+{
+    public class CoreMappingProfile : Profile
+    {
+        public CoreMappingProfile()
+        {
+            //E.g. CreateMap<Resource, ResourceServiceModel>();
+            CreateMap<Resource, ResourceDto>();
+        }
+    }
+}
