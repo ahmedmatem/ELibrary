2aa6e41e451b0a85c0f3fdcf36fb953c1aabc069
Ahmed Matem <ahmedmatem@gmail.com>
2025-09-14 08:45:02 +0300

    Add Automapper in AdminBaseController

--- ELibrary.Web/Areas/Admin/Controllers/AdminBaseController.cs
+++ ELibrary.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+
 using ELibrary.Infrastructure.Data.Types;
 
 namespace ELibrary.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Authorize(Roles = AppRole.ADMIN)]
-    public class AdminBaseController : Controller { }
+    public class AdminBaseController : Controller 
+    {
+        protected readonly IMapper mapper;
+
+        public AdminBaseController(IMapper mapper) 
+        {
+            this.mapper = mapper;
+        }
+    }
 }
--- ELibrary.Web/Areas/Admin/Controllers/HomeController.cs
+++ ELibrary.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+
 namespace ELibrary.Web.Areas.Admin.Controllers
 {
     public class HomeController : AdminBaseController
     {
+        public HomeController(IMapper mapper) : base(mapper)
+        {
+        }
+
         public IActionResult Index()
         {
             return View();
