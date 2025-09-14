b1c41d20f1f4c568c095407f131eea0ea75cf08c
Ahmed Matem <ahmedmatem@gmail.com>
2025-09-14 09:09:30 +0300

    Create BooksController

---
+++ ELibrary.Web/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using AutoMapper;
+
+namespace ELibrary.Web.Areas.Admin.Controllers
+{
+    public class BooksController : AdminBaseController
+    {
+        public BooksController(IMapper mapper) : base(mapper)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
